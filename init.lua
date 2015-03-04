9f16d1395c19c769eb85c402afd49ae5cc47107f
Matthew Luscher <matthew.luscher@greatcall.com>
2015-03-04 12:35:23 -0800

    Updated comments in 'init.lua' and adjusted for being nested

--- init.lua
+++ init.lua
@@ -1,1 +1,5 @@
-require "luaunit"
+-- This is a proxy to call the luaunit library from within eclipse
+-- and also adheres to the 'init' formalism within Eclipse and the 
+-- Lua community.
+
+require "luaunit.luaunit"
